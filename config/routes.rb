39210b1f0795edb1ce7087bc1cfab5787265a1ea
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-15 19:12:29 -0500

    update routes for merchant items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :merchants, only: [:index, :show], module: :merchant do
+    resources :items,  only: [:index, :create, :new]
+  end
 end
