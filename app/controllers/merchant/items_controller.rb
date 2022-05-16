d05efc68bd46a34da501f6c78e5c0a97538c1a0b
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-15 19:12:58 -0500

    feat: create new item from item index page link

---
+++ app/controllers/merchant/items_controller.rb
@@ -0,0 +1,20 @@
+class Merchant::ItemsController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    Item.create!(item_params)
+    redirect_to merchant_items_path(@merchant.id)
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
+end
