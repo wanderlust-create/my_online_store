7578f2e9860f5c4790a0fab71bb65d0d1a33d671
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-17 15:04:31 -0500

    add routes to create and destroy shipment_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :merchants, only: %i[index show], module: :merchant do
     resources :items, only: %i[index show new create edit update destroy]
-    resources :shipments, only: %i[index show new create]
+    resources :shipments, only: %i[index show new create destroy]
+    resources :shipment_items, only: %i[new create destroy]
   end
 end
