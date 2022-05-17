8b77b2af574a74565ac05c8a044c2a81c265ea61
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-16 22:38:25 -0500

    appease Rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_17_033140) do
+ActiveRecord::Schema[7.0].define(version: 20_220_517_033_140) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.float "unit_price"
-    t.bigint "merchant_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["merchant_id"], name: "index_items_on_merchant_id"
+  create_table 'items', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.float 'unit_price'
+    t.bigint 'merchant_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['merchant_id'], name: 'index_items_on_merchant_id'
   end
 
-  create_table "merchants", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'merchants', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "shipment_items", force: :cascade do |t|
-    t.bigint "shipment_id", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_shipment_items_on_item_id"
-    t.index ["shipment_id"], name: "index_shipment_items_on_shipment_id"
+  create_table 'shipment_items', force: :cascade do |t|
+    t.bigint 'shipment_id', null: false
+    t.bigint 'item_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['item_id'], name: 'index_shipment_items_on_item_id'
+    t.index ['shipment_id'], name: 'index_shipment_items_on_shipment_id'
   end
 
-  create_table "shipments", force: :cascade do |t|
-    t.string "name"
-    t.string "state"
-    t.integer "zip_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "address"
+  create_table 'shipments', force: :cascade do |t|
+    t.string 'name'
+    t.string 'state'
+    t.integer 'zip_code'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'address'
   end
 
-  add_foreign_key "items", "merchants"
-  add_foreign_key "shipment_items", "items"
-  add_foreign_key "shipment_items", "shipments"
+  add_foreign_key 'items', 'merchants'
+  add_foreign_key 'shipment_items', 'items'
+  add_foreign_key 'shipment_items', 'shipments'
 end
