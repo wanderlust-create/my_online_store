3e423927e7d56c02ed928dd27909c0e28098325b
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-17 15:04:05 -0500

    feat: find_name and find_unit_price for item

---
+++ app/models/shipment_item.rb
@@ -0,0 +1,13 @@
+class ShipmentItem < ApplicationRecord
+  belongs_to :shipment
+  belongs_to :item
+
+  def find_name
+    Item.find(self.item_id).name
+  end
+
+  def find_unit_price
+    Item.find(self.item_id).unit_price
+  end
+
+end
