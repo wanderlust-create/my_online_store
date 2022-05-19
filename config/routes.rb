97203c4e2e0826603051d35caa32b9146ca9162c
Tamara Dowis <tamara.dowis@gmail.com>
2022-05-19 12:16:09 -0500

    add shipment update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'application#welcome'
   resources :merchants, only: %i[index show], module: :merchant do
     resources :items, only: %i[index show new create edit update destroy]
-    resources :shipments, only: %i[index show new create destroy]
+    resources :shipments, only: %i[index show new create edit update destroy]
     resources :shipment_items, only: %i[new create destroy]
   end
 end
